c88bb4f976870e281a0e65bcb33b626a44316efe
dkamundi <davekamundi@gmail.com>
2023-07-24 15:14:28 -0700

    3. Dictionary of list of dictionaries

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,123 +1,42 @@
 #!/usr/bin/python3
-
 """
-Python script to fetch employee TODO list progress from the given REST API
-and export it to JSON and CSV formats.
+This module gathers data from a REST API to retrieve information
+about the TODO list progress for a given employee ID.
 """
 
-import sys
-import requests
-import csv
 import json
-
-
-def get_employee_todo_progress(employee_id):
-    """
-    Fetches the employee TODO list progress.
-
-    Args:
-        employee_id (int): The ID of the employee.
-
-    Returns:
-        (str, list): A tuple containing the employee name and a list of completed tasks.
-    """
-    base_url = "https://jsonplaceholder.typicode.com"
-    employee_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos?userId={employee_id}"
-
-    try:
-        # Fetching employee information
-        employee_response = requests.get(employee_url)
-        employee_data = employee_response.json()
-
-        # Check if employee exists
-        if "name" not in employee_data:
-            print(f"Error: Employee with ID {employee_id} not found.")
-            sys.exit(1)
-
-        employee_name = employee_data["name"]
-
-        # Fetching TODO list for the employee
-        todos_response = requests.get(todos_url)
-        todos_data = todos_response.json()
-
-        # Extracting completed tasks
-        completed_tasks = [
-            {
-                "task": task["title"],
-                "completed": task["completed"],
-                "username": employee_name
-            }
-            for task in todos_data if task["completed"]
-        ]
-
-        return employee_name, completed_tasks
-
-    except requests.exceptions.RequestException as e:
-        print("Error occurred while fetching data:", e)
-        sys.exit(1)
-    except (KeyError, ValueError):
-        print("Error: Invalid employee ID or invalid API response")
-        sys.exit(1)
-
-
-def export_to_csv(employee_id, employee_name, completed_tasks):
-    """
-    Exports the employee TODO list progress to a CSV file.
-
-    Args:
-        employee_id (int): The ID of the employee.
-        employee_name (str): The name of the employee.
-        completed_tasks (list): List of completed tasks.
-
-    Returns:
-        None
-    """
-    filename = f"{employee_id}.csv"
-    with open(filename, 'w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for task in completed_tasks:
-            csv_writer.writerow([employee_id, employee_name, "true", task["task"]])
-
-    print(f"Data exported to {filename}")
-
-
-def export_to_json(employee_id, completed_tasks):
-    """
-    Exports the employee TODO list progress to a JSON file.
-
-    Args:
-        employee_id (int): The ID of the employee.
-        completed_tasks (list): List of completed tasks.
-
-    Returns:
-        None
-    """
-    data = {str(employee_id): completed_tasks}
-    filename = f"{employee_id}.json"
-    with open(filename, 'w') as json_file:
-        json.dump(data, json_file, indent=2)
-
-    print(f"Data exported to {filename}")
+import requests
+import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_and_export_to_JSON.py <employee_id>")
-        sys.exit(1)
+    """
+    Retrieves the TODO list progress for a given employee ID
+    and prints the employee's name and the number of completed tasks.
+    """
+    # Gets Employee information
+    user_id = int(sys.argv[1])
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    user_response = requests.get(user_url).json()
+    username = user_response.get("username")
+    print(username)
 
-    try:
-        employee_id = int(sys.argv[1])
-        employee_name, completed_tasks = get_employee_todo_progress(employee_id)
-        print(f"Employee {employee_name} is done with tasks ({len(completed_tasks)}/{employee_id}):")
-        for task in completed_tasks:
-            print(f"\t{task['task']}")
+    # Get number of tasks done
+    todo_url = "https://jsonplaceholder.typicode.com/todos"
+    todos_response = requests.get(todo_url).json()
 
-        export_to_csv(employee_id, employee_name, completed_tasks)
-        export_to_json(employee_id, completed_tasks)
+    output = {
+        user_id: [
+            {
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": username
+            }
+            for task in todos_response if task.get("userId") == user_id
+        ]
+    }
 
-    except ValueError:
-        print("Error: Invalid employee ID (must be an integer)")
-        sys.exit(1)
+    # print(output)
 
+    with open("{}.json".format(str(user_id)), "w", encoding="utf-8") as file_:
+        json.dump(output, file_)
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,97 +1,37 @@
 #!/usr/bin/python3
-
 """
-Python script to fetch employee TODO list progress from the given REST API
-and export it to a JSON file.
+This module gathers data from a REST API to retrieve information
+about the TODO list progress for all employees.
 """
 
-import sys
-import requests
 import json
-
-
-def get_employee_todo_progress(employee_id):
-    """
-    Fetches the employee TODO list progress.
-
-    Args:
-        employee_id (int): The ID of the employee.
-
-    Returns:
-        (str, list): A tuple containing the employee name and a list of completed tasks.
-    """
-    base_url = "https://jsonplaceholder.typicode.com"
-    employee_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos?userId={employee_id}"
-
-    try:
-        # Fetching employee information
-        employee_response = requests.get(employee_url)
-        employee_data = employee_response.json()
-
-        # Check if employee exists
-        if "name" not in employee_data:
-            print(f"Error: Employee with ID {employee_id} not found.")
-            sys.exit(1)
-
-        employee_name = employee_data["name"]
-
-        # Fetching TODO list for the employee
-        todos_response = requests.get(todos_url)
-        todos_data = todos_response.json()
-
-        # Extracting completed tasks
-        completed_tasks = [
-            {
-                "username": employee_name,
-                "task": task["title"],
-                "completed": task["completed"]
-            }
-            for task in todos_data
-        ]
-
-        return completed_tasks
-
-    except requests.exceptions.RequestException as e:
-        print("Error occurred while fetching data:", e)
-        sys.exit(1)
-    except (KeyError, ValueError):
-        print("Error: Invalid employee ID or invalid API response")
-        sys.exit(1)
-
-
-def export_to_json(todo_data):
-    """
-    Exports the employee TODO list progress to a JSON file.
-
-    Args:
-        todo_data (dict): Dictionary containing employee ID as key and the list of completed tasks.
-
-    Returns:
-        None
-    """
-    filename = "todo_all_employees.json"
-    with open(filename, 'w') as json_file:
-        json.dump(todo_data, json_file, indent=4)
-
-    print(f"Data exported to {filename}")
+import requests
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 1:
-        print("Usage: python3 gather_data_and_export_to_JSON.py")
-        sys.exit(1)
+    """
+    Retrieves the TODO list progress for all employees
+    and stores the information in a dictionary of lists of dictionaries.
+    """
+    user_url = "https://jsonplaceholder.typicode.com/users"
+    user_response = requests.get(user_url).json()
 
-    try:
-        # Fetching TODO list for all employees and organizing the data by employee ID
-        todo_data = {}
-        for employee_id in range(1, 11):  # Assuming employee IDs are from 1 to 10
-            completed_tasks = get_employee_todo_progress(employee_id)
-            todo_data[str(employee_id)] = completed_tasks
+    todo_url = "https://jsonplaceholder.typicode.com/todos"
+    todos_response = requests.get(todo_url).json()
 
-        export_to_json(todo_data)
+    output = {}
+    for user in user_response:
+        user_id = user.get("id")
+        username = user.get("username")
+        tasks = [
+            {
+                "username": username,
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+            }
+            for task in todos_response if task.get("userId") == user_id
+        ]
+        output[user_id] = tasks
 
-    except ValueError:
-        print("Error: Invalid employee ID (must be an integer)")
-        sys.exit(1)
-
+    with open("todo_all_employees.json", "w", encoding="utf-8") as file_:
+        json.dump(output, file_)
