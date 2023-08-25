2e6df3677fcb992f2ab9c89ab5bed7d8314883be
dkamundi <davekamundi@gmail.com>
2023-08-25 05:19:54 -0700

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,16 +1,12 @@
-# Puppet manifest to fix file extension typo and restart Apache service
-
 # Fix file extension typo using sed
 exec { 'replace phpp with php':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/',
-  require => File['/var/www/html/wp-settings.php'],  # Ensure the file exists before running the command
+  path    => '/usr/local/bin/:/bin/'
 }
 
 # Restart Apache service
-service { 'apache2':
-  ensure  => 'running',
-  enable  => true,
-  require => Exec['replace phpp with php'],  # Restart Apache after fixing the file
+exec { 'restart apache2 service':
+  command => 'service apache2 restart',
+  provider => shell
 }
 
